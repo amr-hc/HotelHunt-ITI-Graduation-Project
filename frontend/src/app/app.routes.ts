99e9bbbc76d45e242c6256563fb8fa3e4dd5a730
mohamed mostafa <muhammed.moustafa.ahmed@gmail.com>
2024-06-24 02:13:11 +0300

    add route for hotel

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { AvailabilityComponent } from './availability/availability.component';
 import { RegisterHotelComponent } from './components/register-hotel/register-hotel.component';
 import { SearchHotelsComponent } from './user/search-hotels/search-hotels.component';
 import { LoginComponent } from './login/login.component';
+import { HotelComponent } from './components/hotel/hotel.component';
 
 export const routes: Routes = [
   {path: 'register', component: RegisterComponent },
   { path: 'availability', component: AvailabilityComponent},
   {path: 'register/hotel',component: RegisterHotelComponent },
   { path: 'search', component: SearchHotelsComponent },
+  { path: 'hotel/:id' , component: HotelComponent}
   { path: 'login', component: LoginComponent },
 ];
