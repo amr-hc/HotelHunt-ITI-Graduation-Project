79d4cf2f682d52a394202d9b6193918a48cca8fd
TalaatEysa <talaateysa@outlook.com>
2024-06-23 23:32:36 +0300

    updating routes in app.routes file

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import { AvailabilityComponent } from './availability/availability.component';
+import { SearchHotelsComponent } from './user/search-hotels/search-hotels.component';
 
 export const routes: Routes = [
 
     {
         path: 'availability',
         component: AvailabilityComponent,
-      },
+  },
+  {
+    path: 'searchhotels',
+    component:SearchHotelsComponent
+  }
 ];
