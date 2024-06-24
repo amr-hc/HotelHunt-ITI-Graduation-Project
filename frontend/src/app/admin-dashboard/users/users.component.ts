d3cc29d07e51f1d0a3c9483a017780c71d905103
ahmeddmahmoud <ahmed.mahmoudd.elshahat@gmail.com>
2024-06-24 22:02:37 +0300

    updating users component & routes

--- frontend/src/app/admin-dashboard/users/users.component.ts
+++ frontend/src/app/admin-dashboard/users/users.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { User } from '../../models/user';
+import { UserService } from '../../services/user.service';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './users.component.html',
-  styleUrl: './users.component.css'
+  styleUrl: './users.component.css',
 })
 export class UsersComponent {
+  users: User[] = [];
+  filteredUsers: User[] = [];
 
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.userService.getAllUsers().subscribe(
+      (response: any) => {
+        this.users = response.data;
+        this.filteredUsers = this.users.filter((user) => user.role === 'guest');
+
+        console.log('Fetched users:', this.users);
+      },
+      (error) => {
+        console.error('Error fetching users', error);
+      }
+    );
+  }
 }
