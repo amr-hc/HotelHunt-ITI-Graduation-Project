87d2b6e9753fcfe8cf25c9f8b21b7b38be591270
mohamed mostafa <muhammed.moustafa.ahmed@gmail.com>
2024-06-25 14:47:36 +0300

    edit in booking component

--- frontend/src/app/models/booking.ts
+++ frontend/src/app/models/booking.ts
@@ -4,8 +4,10 @@ export class Booking {
   constructor(
     public id: number,
     public user_id: number,
+    public user_name:string,
     public duration: number,
     public status: string,
+    public total_price:number,
     public book_details: BookingDetails[]
   ) {}
 }
