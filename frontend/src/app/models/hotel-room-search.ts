799ef6eaca0e8d88b455857d35587994578bfe59
TalaatEysa <talaateysa@outlook.com>
2024-06-25 06:46:44 +0300

    fixing price in hotel room search model

--- frontend/src/app/models/hotel-room-search.ts
+++ frontend/src/app/models/hotel-room-search.ts
@@ -3,7 +3,7 @@ export class HotelRoomSearch {
 
     public id: number,
     public name: string,
-    public price: string,
+    public price: number,
     public stock: number,
     public description: string,
   ) {}
