029013b8b0abba2a8987689e16784848cd9e9f61
shimaa3655 <shimaak137@gmail.com>
2024-06-25 16:21:20 +0300

    static pages

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,12 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {RegisterComponent} from './components/register/register.component';
 import { HotelComponent } from './components/hotel/hotel.component';
+import { HeaderComponent } from './layouts/header/header.component';
+import { SliderComponent } from './layouts/slider/slider.component';
+import { FooterComponent } from './layouts/footer/footer.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,RegisterComponent,HotelComponent],
+  imports: [RouterOutlet,RegisterComponent,HotelComponent,HeaderComponent,SliderComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -9,6 +9,9 @@ import { ListComponent } from './roomtype/list/list.component';
 import { ShowComponent } from './roomtype/show/show.component';
 import { AddComponent } from './roomtype/add/add.component';
 import { UpdateComponent } from './roomtype/update/update.component';
+import { AboutComponent } from './components/about/about.component';
+import { HomeComponent } from './components/home/home.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 export const routes: Routes = [
   {path: 'register', component: RegisterComponent },
@@ -20,9 +23,10 @@ export const routes: Routes = [
   {path : "list" , component : ListComponent},
   {path : "show/:id" , component : ShowComponent },
   {path : "add" , component : AddComponent },
-  {path : "update/:id" , component : UpdateComponent }
-  
-
+  {path : "update/:id" , component : UpdateComponent },
   { path:'admin-dashboard',     loadChildren: () =>
-    import('./admin-dashboard/admin-dashboard-routes').then((m) => m.adminRoutes)}
+    import('./admin-dashboard/admin-dashboard-routes').then((m) => m.adminRoutes)},
+    {path : "home" , component : HomeComponent },
+   {path : "about" , component : AboutComponent },
+   {path : "contact" , component :ContactComponent },
 ];
