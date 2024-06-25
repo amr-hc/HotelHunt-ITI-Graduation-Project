e8d2a852cf4fa57302796b134af313868c38a895
TalaatEysa <talaateysa@outlook.com>
2024-06-25 23:06:37 +0300

    modify comment model

--- frontend/src/app/models/comment.ts
+++ frontend/src/app/models/comment.ts
@@ -1,11 +1,11 @@
 export class Comment {
   constructor(
     public id: number,
-    public hotel_id: number,
-    public user_id: number,
     public content: string,
     public created_at:string,
-    public updated_at:string
+    public user_id: number,
+    public user_name: string,
+    public hotel_id: number,
   ) { }
 }
 
