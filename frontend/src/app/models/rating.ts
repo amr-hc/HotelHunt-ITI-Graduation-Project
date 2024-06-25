e8359ff6ef73b77502f238b0bbba415ac1e2abe2
TalaatEysa <talaateysa@outlook.com>
2024-06-25 22:27:14 +0300

    creating rating model

---
+++ frontend/src/app/models/rating.ts
@@ -0,0 +1,17 @@
+export class Rating {
+  constructor(
+    public id: number,
+    public rate: number,
+    public user_id: number,
+    public user: string,
+    public hotel_id: number,
+    public hotel_name: string
+  ){}
+}
+export class UserRating {
+  constructor(
+    public rate: number,
+    public user_id: number,
+    public hotel_id: number,
+  ){}
+}
