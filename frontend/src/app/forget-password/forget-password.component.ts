f485ea5168647fe7a6bc96833d9dd51fe7008500
mohamed mostafa <muhammed.moustafa.ahmed@gmail.com>
2024-06-27 02:19:38 +0300

    add logic and template for forget password

--- frontend/src/app/forget-password/forget-password.component.ts
+++ frontend/src/app/forget-password/forget-password.component.ts
@@ -1,12 +1,62 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-forget-password',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule,FormsModule],
   templateUrl: './forget-password.component.html',
   styleUrl: './forget-password.component.css'
 })
-export class ForgetPasswordComponent {
+export class ForgetPasswordComponent implements OnInit {
+   email: string = '';
+  password: string = '';
+  passwordConfirmation: string = '';
+  token: string = '';
+  isSubmitting = false;
+  message: string = '';
+
+  constructor(private http: HttpClient, private activatedRoute:ActivatedRoute) {
+  }
+  ngOnInit(): void {
+    this.activatedRoute.queryParams.subscribe(params=>{
+      this.token = params['token'];
+    })
+  }
+
+  onSubmit() {
+    if (!this.email || !this.password || !this.passwordConfirmation) {
+      this.message = 'Please fill in all fields.';
+      return;
+    }
+
+    if (this.password !== this.passwordConfirmation) {
+      this.message = 'Passwords do not match.';
+      return;
+    }
+
+    this.isSubmitting = true;
+    const formData = {
+      email: this.email,
+      password: this.password,
+      password_confirmation: this.passwordConfirmation,
+      token: this.token
+    };
+
+    this.http.post('http://127.0.0.1:8000/api/reset-password', formData)
+      .subscribe(
+        response => {
+          this.message = 'Password reset successful!';
+          this.isSubmitting = false;
+        },
+        error => {
+          this.message = 'Failed to reset password. Please try again.';
+          this.isSubmitting = false;
+        }
+      );
+  }
 
 }
