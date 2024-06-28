73fd95672ac7111604321284c88bd75be14ea780
mohamed mostafa <muhammed.moustafa.ahmed@gmail.com>
2024-06-29 00:31:49 +0300

    add confirmation message for delete at room type

--- frontend/src/app/roomtype/list/list.component.ts
+++ frontend/src/app/roomtype/list/list.component.ts
@@ -3,6 +3,7 @@ import { RoomtypeService } from '../../services/roomtype.service';
 import { RoomType } from '../../models/roomtype';
 import { CommonModule } from '@angular/common';
 import { RouterModule,Router} from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-list',
@@ -29,19 +30,42 @@ export class ListComponent {
     this.showData();
   }
 
-  remove(id : any) {
-    this.serv.delete(id).subscribe(
-      (data : any ) => {
-        this.message = " Delete Successfully " ;
-        setTimeout(() => {
-          this.message = ""
-        }, 2000 );
-      }
-    );
+  remove(id: any) {
+    // Show the SweetAlert2 confirmation dialog
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            // Proceed with the deletion if confirmed
+            this.serv.delete(id).subscribe(
+                (data: any) => {
+                    this.message = "Deleted Successfully";
+                    setTimeout(() => {
+                        this.message = "";
+                        this.showData(); // Refresh the data
+                    }, 2000);
+                },
+                (error) => {
+                    console.error('Error deleting item:', error);
+                    this.message = 'Error deleting item. Please try again.';
+                }
+            );
 
-    this.showData();
-
-  }
+            // Show a success message using SweetAlert2
+            Swal.fire(
+                'Deleted!',
+                'Your file has been deleted.',
+                'success'
+            );
+        }
+    });
+}
 
   addRoomType() {
     this.router.navigate(['/owner/add']);
