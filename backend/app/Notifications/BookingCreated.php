0d1ad9a281426afe9e6a93a8c83e2f20f8b2e05b
amr-hc <amro7275@gmail.com>
2024-06-29 00:08:30 +0300

    send email booking reverstion

---
+++ backend/app/Notifications/BookingCreated.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class BookingCreated extends Notification implements ShouldQueue
+{
+    use Queueable;
+    public $booking;
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct($booking)
+    {
+        $this->booking = $booking;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+        ->subject('Booking Confirmation')
+        ->view('booking_created', ['booking' => $this->booking ]);
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            // 'booking_id' => $this->booking->id,
+            // 'booking_date' => $this->booking->created_at,
+        ];
+    }
+}
