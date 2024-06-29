f57e54b19070bffb779e2555723fdaf9457b1694
shimaa3655 <shimaak137@gmail.com>
2024-06-29 10:45:47 +0300

    edit on create roomtype

--- frontend/src/app/roomtype/show/show.component.ts
+++ frontend/src/app/roomtype/show/show.component.ts
@@ -1,41 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RoomtypeService } from '../../services/roomtype.service';
 import { RoomType } from '../../models/roomtype';
-import { CommonModule } from '@angular/common';
 import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-show',
-  standalone: true,
-  imports: [CommonModule],
   templateUrl: './show.component.html',
-  styleUrls: ['./show.component.css'],
+  styleUrls: ['./show.component.css']
 })
-export class ShowComponent {
-  id!: any;
+export class ShowComponent implements OnInit {
+  id: any;
   object: RoomType = {
     id: 0,
     name: '',
     price: 0,
     description: '',
     capacity: 0,
-    photo: '',
+    photo: ''
   };
 
   constructor(
-    private serv: RoomtypeService,
-    private activatedRoute: ActivatedRoute,
+    private roomtypeService: RoomtypeService,
+    private route: ActivatedRoute,
     private router: Router
-  ) {
-    this.id = this.activatedRoute.snapshot.paramMap.get('id');
-    this.serv.getById(this.id).subscribe((data: any) => {
-      console.log(data);
-      this.object = data;
-      console.log("image", this.object.photo);
-    });
+  ) {}
+
+  ngOnInit(): void {
+    this.id = this.route.snapshot.paramMap.get('id');
+    this.roomtypeService.getById(this.id).subscribe(
+      (data: RoomType) => {
+        this.object = data;
+        console.log('Fetched RoomType:', this.object);
+      },
+      (error) => {
+        console.error('Error fetching RoomType:', error);
+      }
+    );
   }
 
-  reset() {
+  goBack(): void {
     this.router.navigate(['/owner/list']);
   }
 }
