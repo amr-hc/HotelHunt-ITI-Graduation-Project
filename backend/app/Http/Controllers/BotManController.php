3d0d571396bd82770ed758940a5f7bec634a8633
amr-hc <amro7275@gmail.com>
2024-07-20 20:27:37 +0300

    Enhance Botmant

--- backend/app/Http/Controllers/BotManController.php
+++ backend/app/Http/Controllers/BotManController.php
@@ -1,56 +1,62 @@
 <?php
+
 namespace App\Http\Controllers;
-   
+
 use BotMan\BotMan\BotMan;
 use Illuminate\Http\Request;
 use BotMan\BotMan\Messages\Incoming\Answer;
-   
+use BotMan\BotMan\Messages\Conversations\Conversation;
+use App\Models\Hotel;
+
 class BotManController extends Controller
 {
-    /**
-     * Place your BotMan logic here.
-     */
     public function handle()
     {
         $botman = app('botman');
    
         $botman->hears('{message}', function($botman, $message) {
-   
             if ($message == 'hi') {
-                $this->askName($botman);
+                $botman->startConversation(new WelcomeConversation);
+            } else {
+                $botman->reply("Start by saying 'hi'.");
             }
-            
-            else{
-                $botman->reply("Start saying hi.");
-            }
-   
         });
    
         $botman->listen();
     }
-   
-    /**
-     * Place your BotMan logic here.
-     */
-    public function askName($botman)
-    {
-        $botman->ask('Hello! What is your Name?', function(Answer $answer, $conversation) {
-            $name = $answer->getText();
-   
-            $this->say('Welcome to HotelHunt '.$name);
-            $this->say('I am going to help you to receive room');
+}
 
-            $conversation->ask('which city do you like booking in ?', function(Answer $answer, $conversation) {
-   
-                $name = $answer->getText();
-           
-                $this->say('will search about this city '.$name);
-            });
-            
+class WelcomeConversation extends Conversation
+{
+    public function askName()
+    {
+        $this->ask('Hello! What is your name?', function(Answer $answer) {
+            $name = $answer->getText();
+            $this->say('Welcome to HotelHunt, ' . ucfirst($name) . ' !');
+            $this->askCity();
         });
     }
 
+    public function askCity()
+    {
+        $this->ask('Which city would you like to book in?', function(Answer $answer) {
+            $city = $answer->getText();
+            $hotels = Hotel::where('city', ucfirst($city))->get();
 
+            if ($hotels->isEmpty()) {
+                $this->say('Sorry, no hotels found in ' . $city);
+                $this->askCity();
+            } else {
+                $this->say('Found ' . $hotels->count() . ' hotels in ' . $city);
+                foreach ($hotels as $hotel) {
+                    $this->say('<a href="http://localhost:4200/hotel/' . $hotel->id . '" target="_blank">' . $hotel->name . '</a>');
+                }
+            }
+        });
+    }
 
-
+    public function run()
+    {
+        $this->askName();
+    }
 }
